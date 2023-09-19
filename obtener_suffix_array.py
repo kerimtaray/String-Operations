2a5f0fad677ce62ddb0ea0daab52f4a18c18b78a
galactickem <a00827581@tec.mx>
2023-09-18 23:14:08 -0600

    Adding suffix array

---
+++ obtener_suffix_array.py
@@ -0,0 +1,21 @@
+def obtener_suffix_array(cadena):
+    sufijos = [
+        (cadena[i:], i) for i in range(len(cadena))
+    ]  # se crea una lista de todos los sufijos
+
+    sufijos.sort(key=lambda x: x[0])  # se ordena la lista de sufijos
+
+    suffix_array = [
+        indice for _, indice in sufijos
+    ]  # se crea un array con los indices iniciales de los sufijos ordenados
+
+    return suffix_array
+
+
+def main():
+    cadena = "banana"
+    print("Cadena:", cadena)
+    print("Suffix Array:", obtener_suffix_array(cadena))
+
+
+main()
